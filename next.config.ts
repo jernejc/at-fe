022b955b231a32fb2c39cae3f6679b742aecdcc7
Jernej <jernej.co@gmail.com>
2026-02-13 09:48:04 +0100

    unoptimized images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -11,7 +12,6 @@ const nextConfig: NextConfig = {
     ],
   },
   serverExternalPackages: ["firebase-admin"],
-  output: "standalone",
 };
 
 export default nextConfig;
